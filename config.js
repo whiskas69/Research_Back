65e117497183c3e93969b8e9a03092148b472b52
whiskas69 <64070075@it.kmail.ac.th>
2025-02-18 23:25:55 +0700

    erer

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'root',//แก้ด้วยๆๆๆ
+  password: 'wine1234',//แก้ด้วยๆๆๆ
   database: 'ResearchAdministration',
   waitForConnections: true,
   connectionLimit: 10,
