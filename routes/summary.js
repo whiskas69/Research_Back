10b4a5ce582046326cba92c21ea60ca05351cf8d
mymoon69 <64070105@it.kmitl.ac.th>
2025-03-23 22:41:16 +0700

    update database

--- routes/summary.js
+++ routes/summary.js
@@ -30,4 +30,59 @@ router.get("/all_summary_conference", async (req, res) => {
   }
 });
 
+router.get("/all_summary_page_charge", async (req, res) => {
+  try {
+    const [Summary] = await db.query(
+      `SELECT
+        p.pageC_id,
+        u.user_nameth,
+        p.article_title,
+        p.journal_name,
+        p.quality_journal,
+        p.qt_isi,
+        p.qt_sjr,
+        p.qt_scopus,
+        p.month,
+        p.year,
+        p.article_research_ject,
+        p.research_type,
+        p.request_support,
+        f.form_status
+        FROM Page_Charge p
+        JOIN users u ON p.user_id = u.user_id
+        LEFT JOIN form f ON p.pageC_id = f.pageC_id
+        WHERE f.form_status = "อนุมัติ";`
+    );
+    res.status(200).json(Summary);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/all_summary_kris", async (req, res) => {
+  try {
+    const [Summary] = await db.query(
+      `SELECT
+        k.kris_id,
+        u.user_nameth,
+        k.name_research_th,
+        k.research_cluster,
+        k.res_cluster_other,
+        k.res_standard,
+        k.res_standard_trade,
+        k.year,
+        k.project_periodStart,
+        k.project_periodEnd,
+        f.form_status
+        FROM Research_KRIS k
+        JOIN users u ON k.user_id = u.user_id
+        LEFT JOIN form f ON k.kris_id = f.kris_id
+        WHERE f.form_status = "อนุมัติ";`
+    );
+    res.status(200).json(Summary);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 exports.router = router;
