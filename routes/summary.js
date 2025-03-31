1f17d4f1ac7e593be2bd8baf14df48239ce202a9
unknown <64070105@it.kmitl.ac.th>
2025-03-31 23:31:30 +0700

    complete

--- routes/summary.js
+++ routes/summary.js
@@ -350,29 +350,16 @@ router.get("/count_confer_thai", async (req, res) => {
 
 router.get("/eachyears", async (req, res) => {
   const [Summary] = await db.query(
-    `SELECT * FROM form`
-//     `
-//     SELECT 
-//     b.budget_year,
-//     SUM(CASE WHEN TRMI(f.form_type) = 'Conference' THEN 1 ELSE 0 END) AS confer_count,
-//     SUM(CASE WHEN TRIM(f.form_type) = 'Page_Charge' THEN 1 ELSE 0 END) AS pc_count
-// FROM form f
-// JOIN budget b ON f.form_id = b.form_id
-// WHERE TRIM(f.form_status) = 'อนุมัติ'
-// GROUP BY b.budget_year
-// ORDER BY b.budget_year DESC;
-// `
-//     `SELECT 
-//     b.budget_year,
-//     SUM(CASE WHEN f.form_type = 'Conference' THEN 1 ELSE 0 END) AS confer_count,
-//     SUM(CASE WHEN f.form_type = 'Page_Charge' THEN 1 ELSE 0 END) AS pc_count
-// FROM form f
-// JOIN budget b ON f.form_id = b.form_id
-// WHERE b.budget_year >= YEAR(CURDATE()) - 2
-// AND f.form_status = 'อนุมัติ'
-// GROUP BY b.budget_year
-// ORDER BY b.budget_year DESC;
-// `
+    `SELECT b.budget_year,
+    COALESCE(SUM(CASE WHEN f.form_type = 'Conference' THEN 1 ELSE 0 END), 0) AS total_conferences,
+    COALESCE(SUM(CASE WHEN f.form_type = 'Page_Charge' THEN 1 ELSE 0 END), 0) AS total_pagecharge,
+    COALESCE(SUM(b.Page_Charge_amount), 0) AS total_pagecharge_amount,
+    COALESCE(SUM(b.Conference_amount), 0) AS total_conference_amount
+    FROM budget b
+    LEFT JOIN form f ON b.form_id = f.form_id
+    WHERE b.budget_year >= (YEAR(CURRENT_DATE) - 3) AND f.form_status = "อนุมัติ"
+    GROUP BY b.budget_year
+    ORDER BY b.budget_year DESC;`
   );
 
   res.status(200).json(Summary);
