ef8618bfd68c45e5569436996757d555f8e640f2
mymoon69 <64070105@it.kmitl.ac.th>
2025-05-31 20:46:56 +0700

    config sasikan

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: ' root',//อย่าลืมแก้
+  password: 'root',//อย่าลืมแก้
   database: 'ResearchAdministration',
   waitForConnections: true,
   connectionLimit: 10,
