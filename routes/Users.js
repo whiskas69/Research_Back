a85989c9ba248560bff520dec2b0aaf0b4123c16
whiskas69 <64070075@it.kmail.ac.th>
2025-06-10 22:09:49 +0700

    config

--- routes/Users.js
+++ routes/Users.js
@@ -259,8 +259,8 @@ router.post("/testlogin", async (req, res) => {
 
     //setting Secure Cookie (httpOnly Protect attack XSS)
     res.cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // ต้องเป็น true ถ้าใช้ HTTPS
+      httpOnly: false,
+      secure: false, // ต้องเป็น true ถ้าใช้ HTTPS
       sameSite: "Lax",
       maxAge: 3600000, // 1 hour
     });
