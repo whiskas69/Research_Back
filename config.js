e452eec857e352c2255bd0c6c07aa4f3750e6c84
user03 user <user03@digitalu.it.kmitl.ac.th>
2025-06-16 21:20:41 +0700

    save local changes

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
+<<<<<<< HEAD
   // host: 'mysql', password: 'wine1234', //server
   host: 'localhost',
+=======
+  host: 'mysql',
+>>>>>>> cd9f0c0 (save local changes)
   user: 'root',
-  password: 'root',//อย่าลืมแก้
+  password: 'wine1234',//อย่าลืมแก้
+  charset: 'utf8mb4',
   database: 'ResearchAdministration',
   waitForConnections: true,
   connectionLimit: 10,
